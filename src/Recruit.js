103f8daa1eeffcd8caebaee51cb24eb161ee6fcb
Wonjae Jo <inqueue0979@gmail.com>
2024-02-10 00:22:26 +0900

    Added react-notion (temporary)

--- src/Recruit.js
+++ src/Recruit.js
@@ -1,19 +1,37 @@
 import './App.css';
-import './output.css'
+import './output.css';
+import 'react-notion/src/styles.css';
+
+import { NotionRenderer } from 'react-notion';
+import { useState, useEffect } from 'react';
+
 
 function Recruit()
 {
+
+  const [response, setResponse] = useState({});
+
+  
+  useEffect(() => {
+    // 두번째 방법
+    const NOTION_PAGE_ID = '284a97d63cfc402cbf2b5945695068a9';
+    fetch(`https://notion-api.splitbee.io/v1/page/${NOTION_PAGE_ID}`)
+      .then(res => res.json())
+      .then((resJson) => {
+        setResponse(resJson);
+      });
+  }, [])
+  
   return (
-    <div className="App-header items-center">
-        <div className='text-center w-1/3 md:w-1/4'>
-        <img className='w-full' alt='brick' src='img/rulernBrush.png'></img>
-      </div>
-      <div className='font-black text-2xl text-center tracking-tight md:text-4xl'>
-        <h1>KOSS RECRUITING 2024</h1>
-        <h1>소프트웨어융합대학 학술동아리 KOSS</h1>
-      </div>
+    <div className="App bg-white mt-20 ">
+        <NotionRenderer
+          disableHeader
+          // blockMap={staticResponse}
+          blockMap={response}
+          fullPage={true}
+        />
     </div>
-  )
+  );
 }
 
 export default Recruit;
