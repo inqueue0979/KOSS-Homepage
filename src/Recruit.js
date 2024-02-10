b70c6a5c1bfac9d7717925d0711948a78d3852b6
Wonjae Jo <inqueue0979@gmail.com>
2024-02-10 18:54:54 +0900

    Discard Notion embedding

--- src/Recruit.js
+++ src/Recruit.js
@@ -1,35 +1,14 @@
 import './App.css';
 import './output.css';
-import 'react-notion/src/styles.css';
-
-import { NotionRenderer } from 'react-notion';
-import { useState, useEffect } from 'react';
 
 
 function Recruit()
 {
-
-  const [response, setResponse] = useState({});
-
-  
-  useEffect(() => {
-    // 두번째 방법
-    const NOTION_PAGE_ID = '284a97d63cfc402cbf2b5945695068a9';
-    fetch(`https://notion-api.splitbee.io/v1/page/${NOTION_PAGE_ID}`)
-      .then(res => res.json())
-      .then((resJson) => {
-        setResponse(resJson);
-      });
-  }, [])
   
   return (
-    <div className="App bg-white mt-20 ">
-        <NotionRenderer
-          disableHeader
-          // blockMap={staticResponse}
-          blockMap={response}
-          fullPage={true}
-        />
+    <div className="App-header bg-stone-950">
+      <h1>학술 활동과 학교 생활의 즐거움 모두를 손에 쥘 준비가 되셨나요?</h1> <br></br>
+      <button className='w-1/6 min-w-40 py-4 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm'>2024-1 KOSS 지원하기</button>
     </div>
   );
 }
