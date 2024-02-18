302effb698a8b895d7fee140e1606e9c59473678
jhj04 <129846546+jhj04@users.noreply.github.com>
2024-02-18 18:52:38 +0000

    Add Rectuit Page Button Margin

--- src/pages/Recruit.js
+++ src/pages/Recruit.js
@@ -8,7 +8,7 @@ function Recruit()
   return (
       <div className = "App-header bg-stone-950">
         <h1>학술 활동과 학교 생활의 즐거움 모두를 손에 쥘 준비가 되셨나요?</h1>
-        <button className='w-1/6 min-w-40 py-4 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm'>2024-1 KOSS 지원하기</button>
+        <button className='w-1/6 min-w-40 py-4 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm' style={{marginTop: 20}}>2024-1 KOSS 지원하기</button>
         <div class="bubble b1"></div>
         <div class="bubble b2"></div>
         <div class="bubble b3"></div>
