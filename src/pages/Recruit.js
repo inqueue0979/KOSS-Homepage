c98e1cfec494e14b039a0f593254cd0f98ad5627
jhj04 <129846546+jhj04@users.noreply.github.com>
2024-02-18 16:27:43 +0000

    Add Recruit Page animation

--- src/pages/Recruit.js
+++ src/pages/Recruit.js
@@ -6,10 +6,18 @@ function Recruit()
 {
   
   return (
-    <div className="App-header bg-stone-950">
-      <h1>학술 활동과 학교 생활의 즐거움 모두를 손에 쥘 준비가 되셨나요?</h1> <br></br>
-      <button className='w-1/6 min-w-40 py-4 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm'>2024-1 KOSS 지원하기</button>
-    </div>
+      <div className = "App-header bg-stone-950">
+        <h1>학술 활동과 학교 생활의 즐거움 모두를 손에 쥘 준비가 되셨나요?</h1>
+        <button className='w-1/6 min-w-40 py-4 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm'>2024-1 KOSS 지원하기</button>
+        <div class="bubble b1"></div>
+        <div class="bubble b2"></div>
+        <div class="bubble b3"></div>
+        <div class="bubble b4"></div>
+        <div class="bubble b5"></div>
+        <div class="bubble b6"></div>
+        <div class="bubble b7"></div>
+      </div>
+  
   );
 }
 
